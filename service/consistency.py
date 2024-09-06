8d1b25fb0f8cc1527e33eab07e65568ca2402e3a
qawesrdtfy <2770278140@qq.com>
2024-09-06 21:30:16 +0800

    "触发函数要求判断参数内容，数据集的读取功能"

--- service/consistency.py
+++ service/consistency.py
@@ -1,21 +1,30 @@
 import numpy as np
 from statsmodels.stats.inter_rater import fleiss_kappa
 from rouge_chinese import Rouge
+from skimage.metrics import structural_similarity as ssim
 from ..tools.funcs import *
+from ..config.Data import Data
 rouge=Rouge()
 
 
-def trig_class_consistency(Y_modal=[], Y_per_annotater=[]) -> bool:
+def trig_class_consistency(data:Data) -> bool:
     """
     类别一致性的触发函数
     :param Y_modal: Y的模态
     :param Y_per_annotater: 每个样本每个标注员的标注结果
     :return: 是否触发，bool
     """
-    if len(Y_modal)==1 and Y_modal[0]=='类别' and len(Y_per_annotater)!=0:
-        return True
+    if len(data.Y_modal)==1 and data.Y_modal[0]=='类别':
+        if len(data.Y_per_annotater)!=0:
+            # 要求每个样本的标注员数量一样
+            annotater_count=len(data.Y_per_annotater[0])
+            for sample in data.Y_per_annotater:
+                if annotater_count!=len(sample):
+                    break
+            else:
+                return True
     return False
-def class_consistency(Y_per_annotater:list):
+def class_consistency(data:Data):
     """
     类别一致性
     :param Y_per_annotater: 每个样本每个标注员的标注结果
@@ -23,13 +32,13 @@ def class_consistency(Y_per_annotater:list):
     """
     class_list=[]
     # 统计都有哪些类
-    for sample in Y_per_annotater:
+    for sample in data.Y_per_annotater:
         for annotated in sample:
             if annotated not in class_list:
                 class_list.append(annotated)
     # 统计每个样本每个类的支持人数
     class_statistic=[]
-    for sample in Y_per_annotater:
+    for sample in data.Y_per_annotater:
         line=[0 for _ in class_list]
         for annotated in sample:
             line[class_list.index(annotated)]+=1
@@ -41,36 +50,83 @@ def class_consistency(Y_per_annotater:list):
     return kappa
 
 
-def trig_content_consistency(Y_modal=[], Y_per_annotater=[]) -> bool:
+def trig_content_consistency(data:Data) -> bool:
     """
     内容一致性的触发函数
     :param Y_modal: Y的模态
     :param Y_per_annotater: 每个样本每个标注员的标注结果
     :return: 是否触发，bool
     """
-    if len(Y_modal)==1 and Y_modal[0]=='文本' and len(Y_per_annotater)!=0:
-        return True
+    if len(data.Y_modal)==1 and data.Y_modal[0]=='文本':
+        if len(data.Y_per_annotater)!=0:
+            # 要求每个样本每个标注员标注的都是字符串类型
+            for sample in data.Y_per_annotater:
+                for per_annotate in sample:
+                    if not isinstance(per_annotate,str):
+                        break
+                else:
+                    continue
+                break
+            else:
+                return True
     return False
-def content_consistency(Y_per_annotater:list):
+def content_consistency(data:Data):
     """
     内容一致性
     :param Y_per_annotater: 每个样本每个标注员的标注结果
     :return: 内容一致性得分，范围0～1
     """
     all_scores=[]
-    for sample in Y_per_annotater:
+    for sample in data.Y_per_annotater:
         scores=[]
         sample_splited=[' '.join(list(one)) for one in sample]
-        for i,annotated1 in enumerate(sample_splited):
-            for j,annotated2 in enumerate(sample_splited):
-                if i==j:
-                    continue
-                scores.append(rouge.get_scores(annotated1,annotated2)[0]['rouge-l']['f'])
+        for i in range(len(sample_splited)):
+            for j in range(i+1,len(sample_splited)):
+                scores.append(rouge.get_scores(sample_splited[i],sample_splited[j])[0]['rouge-l']['f'])
         score=sum(scores)/len(scores)
         all_scores.append(score)
     return round(sum(all_scores)/len(all_scores),4)
 
 
+def trig_visual_consistency(data:Data) -> bool:
+    """
+    视觉一致性的触发函数
+    :param Y_modal: Y的模态
+    :param Y_per_annotater: 每个样本每个标注员的标注结果
+    :return: 是否触发，bool
+    """
+    if len(data.Y_modal)==1 and data.Y_modal[0]=='图像':
+        if len(data.Y_per_annotater)!=0:
+            # 要求每个样本每个标注员标注的都是numpy矩阵类型，并且尺寸相同
+            shape=data.Y_per_annotater[0][0].shape
+            for sample in data.Y_per_annotater:
+                for per_annotate in sample:
+                    if not isinstance(per_annotate,np.ndarray) or per_annotate.shape!=shape:
+                        break
+                else:
+                    continue
+                break
+            else:
+                return True
+        return True
+    return False
+def visual_consistency(data:Data):
+    """
+    视觉一致性
+    :param Y_per_annotater: 每个样本每个标注员的标注结果
+    :return: 内容一致性得分，范围0～1
+    """
+    all_scores=[]
+    for sample in data.Y_per_annotater:
+        scores=[]
+        for i in range(len(sample)):
+            for j in range(i+1,len(sample)):
+                scores.append(ssim(sample[i], sample[j], multichannel=True))
+        score=sum(scores)/len(scores)
+        all_scores.append(score)
+    return round(sum(all_scores)/len(all_scores),4)
+
 # 函数列表，元素为[指标名，触发函数，计算函数]
-diversity_funclist=[["类别一致性",trig_class_consistency,class_consistency],
-                    ["主题多样性",trig_content_consistency,content_consistency],]
+consistency_funclist=[["类别一致性",trig_class_consistency,class_consistency],
+                    ["内容一致性",trig_content_consistency,content_consistency],
+                    ["视觉一致性",trig_visual_consistency,visual_consistency],]
--- service/diversity.py
+++ service/diversity.py
@@ -1,42 +1,43 @@
 from ..tools.funcs import *
+from ..config.Data import Data
 
-def trig_class_diversity(Y_modal=[]) -> bool:
+def trig_class_diversity(data:Data) -> bool:
     """
     类别多样性的触发函数
     :param Y_modal: Y的模态
     :return: 是否触发，bool
     """
-    if len(Y_modal)==1 and Y_modal[0]=='类别':
+    if len(data.Y_modal)==1 and data.Y_modal[0]=='类别':
         return True
     return False
-def class_diversity(Y:list):
+def class_diversity(data:Data):
     """
     类别多样性
     :param Y: 每个样本的类别
     :return: 类别多样性得分，范围0～1
     """
-    probs=spread_probs(Y)
+    probs=spread_probs(data.Y)
     score,therange=shannon_entropy(probs)
     score=zoom(score,therange[0],therange[1])
     return score
 
 
-def trig_topic_diversity(X_topic=[]) -> bool:
+def trig_topic_diversity(data:Data) -> bool:
     """
     主题多样性的触发函数
     :param X_topic: 每个样本的主题
     :return: 是否触发，bool
     """
-    if len(X_topic)==0:
+    if len(data.X_topic)==0:
         return False
     return True
-def topic_diversity(X_topic:list):
+def topic_diversity(data:Data):
     """
     主题多样性
     :param X_topics: 每个样本的主题
     :return: 主题多样性得分，范围0～1
     """
-    probs=spread_probs(X_topic)
+    probs=spread_probs(data.X_topic)
     score,therange=shannon_entropy(probs)
     score=zoom(score,therange[0],therange[1])
     return score
