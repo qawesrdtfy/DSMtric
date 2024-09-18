9618b796c289dabe1de1d28b13f4b73d21aedc3f
scadule <2177244381@qq.com>
2024-09-18 19:33:33 +0800

    编写词汇丰富度指标-基尼系数

--- service/diversity.py
+++ service/diversity.py
@@ -1,5 +1,6 @@
 from tools.funcs import *
 from config.Data import Data
+from collections import Counter
 
 def trig_class_diversity(data:Data) -> bool:
     """
@@ -96,6 +97,36 @@ def vocabulary_diversity(data:Data):
 
     # 计算并返回 STTR
     return sum(total_ttr_values) / total_segments if total_segments > 0 else 0
+
+
+def trig_vocabulary_richness(data:Data) ->bool:
+    """
+    词汇丰富度的触发函数
+    :param X_modal: 每个样本的模态，要求是文本
+    :return: 是否触发，bool
+    """
+    if len(data.X_modal)==["文本"]:
+        return False
+    return True
+
+def vocabulary_richness(data:Data):
+    """
+    计算文本的基尼系数 其思想是将词频分布类比为收入分布，于是可以计算基尼系数
+    :param X: 每个样本的文本
+    :return: 数据集的基尼系数  取值范围0-1  越接近0表示越均衡
+    """
+    texts=data.X['文本']
+    global_word_counts = Counter()
+    # 对每个文本分别进行分词并统计词频
+    for text in texts:
+        words = jieba.cut(text)
+        word_counts = Counter(words)
+        global_word_counts.update(word_counts)
+    # Step 2: 计算全局基尼系数
+    frequencies = np.array(list(global_word_counts.values()))
+    score = compute_gini(frequencies)
+    return score
+
 
 # 函数列表，元素为[指标名，触发函数，计算函数]
 diversity_funclist=[["类别多样性",trig_class_diversity,class_diversity],
